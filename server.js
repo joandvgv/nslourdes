7257a609ff86d7c2f8719385b566e4644054bc0a
Joan Gil <Joan Gil>
2017-03-21 01:05:12 -0400

    added cors

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ var Logs     = require('./models/logs');
 
 
 var corsOptions = {
-  origin: 'https://acseg.heroku.com',
+  origin: 'http://localhost:4200',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 app.use(cors(corsOptions));
